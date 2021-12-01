417649630d907d72cb53dbe197f1de37afc5b856
Steven Davis <steven.davis@dialexa.com>
2021-11-30 23:03:14 -0800

    moving packages

--- src/one/Day01.kt
+++ src/one/Day01.kt
@@ -4,18 +4,37 @@ import readInput
 
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val heights = input.map { it.toInt() }.toMutableList()
+        var count = 0
+        var lastHeight = heights.removeAt(0)
+
+        heights.forEach {
+            if(it > lastHeight) count += 1
+            lastHeight = it
+        }
+
+        return count
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val heights = input.map { it.toInt() }.toMutableList()
+        var count = 0
+
+        heights.forEachIndexed { index, current ->
+            val next = heights.getOrNull(index + 3)
+            next?.let {
+                if (next > current) count += 1
+            }
+        }
+
+        return count
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInput("one/Day01_test")
+    check(part2(testInput) == 6)
 
-    val input = readInput("one")
-    println(part1(input))
+    val input = readInput("one/Day01")
     println(part2(input))
+//    println(part2(input))
 }
