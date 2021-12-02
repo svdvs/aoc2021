18ac8b8732e8fa81666f2b80fa9cf9df32c5f961
Steven Davis <steven.davis@dialexa.com>
2021-12-02 10:37:32 -0800

    day02

---
+++ src/two/Day02.kt
@@ -0,0 +1,57 @@
+package two
+
+import readInput
+
+enum class Command(val input: String) {
+    FORWARD("forward"),
+    DOWN("down"),
+    UP("up");
+
+    companion object {
+        private val map = Command.values().associateBy(Command::input)
+        fun fromString(input: String): Command = map[input]!!
+    }
+}
+
+data class NavCommand(val command: Command, val amount: Int) {}
+
+class Position(var xPos: Int = 0, var yPos: Int = 0, var aim: Int = 0) {
+    fun processNavigationCommands(commands: List<NavCommand>) : Int {
+        commands.forEach {
+            when(it.command) {
+                Command.FORWARD -> {
+                    xPos += it.amount
+                    yPos += (aim * it.amount)
+                }
+                Command.DOWN -> aim += it.amount
+                Command.UP -> aim -= it.amount
+            }
+        }
+
+        return findCurrentPosition()
+    }
+
+    fun findCurrentPosition(): Int {
+        return xPos * yPos
+    }
+}
+
+fun main() {
+    fun part1(input: List<String>): Int {
+        val regex = """(\w+) (\d+)""".toRegex()
+
+        val commands = input.map {
+            val (command, amount) = regex.find(it)!!.destructured
+            NavCommand(Command.fromString(command), amount.toInt())
+        }
+
+        return Position().processNavigationCommands(commands)
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("two/Day02_test")
+    check(part1(testInput) == 900)
+
+    val input = readInput("two/Day02")
+    println(part1(input))
+}
